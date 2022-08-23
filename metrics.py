43cda3c764994a6722a60cfeaee5cb642ba3ecf9
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-08-23 10:52:13 +0200

    .

--- metrics.py
+++ metrics.py
@@ -22,6 +22,8 @@ def acc(y_true, y_pred):
     w = np.zeros((D, D), dtype=np.int64)
     for i in range(y_pred.size):
         w[y_pred[i], y_true[i]] += 1
-    from sklearn.utils.linear_assignment_ import linear_assignment
+    from scipy.optimize import linear_sum_assignment as linear_assignment
     ind = linear_assignment(w.max() - w)
-    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
+    #print(ind)
+    #return sum([w[i, j] for i, j in zip(ind[0], ind[1])]) * 1.0 / y_pred.size
+    return w[ind[0], ind[1]].sum() / y_pred.size
